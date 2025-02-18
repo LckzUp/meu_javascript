e75bb04869976f8165b9c6fdaaad85904e38dcd8
LckzUp <160252761+LckzUp@users.noreply.github.com>
2025-02-18 07:36:49 -0300

    JavaScript

---
+++ Testes/script.js
@@ -0,0 +1,10 @@
+var nome = prompt("Digite seu Nome");
+var endereco = prompt("Digite seu Endereço");
+var numero_cartao = prompt("Digite seu Número de Cartão de Crédito");
+var senha = prompt("Digite sua Senha");
+
+
+console.log(nome);
+console.log(endereco);
+console.log(numero_cartao);
+console.log(senha);
